e47b0567d87b775a8dc1aaf94f2ec7c0bf62ea5e
dormenog <dormenog@gmail.com>
2018-07-16 23:22:43 +1000

    Initial dist subtree commit

---
+++ precache-manifest.bc885945015f9641af437bc49412eea4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "808250927e30cb8e8c99",
+    "url": "/js/chunk-vendors.80825092.js"
+  },
+  {
+    "revision": "856595eddf6bb8fa03b2",
+    "url": "/js/app.856595ed.js"
+  },
+  {
+    "revision": "771c54734718c5e779de93950c9dbf60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "808250927e30cb8e8c99",
+    "url": "/css/chunk-vendors.159878e2.css"
+  },
+  {
+    "revision": "856595eddf6bb8fa03b2",
+    "url": "/css/app.5690954e.css"
+  }
+];
